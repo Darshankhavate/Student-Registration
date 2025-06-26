f6c814ce71ea2dac002b5266af6459fda9d1575c
Darshankhavate <darshankhavate@gmail.com>
2025-06-26 20:22:54 +0530

    models files

---
+++ app/Models/student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Traits\Timestamp;
+use Illuminate\Database\Eloquent\Model;
+
+class student extends Model
+{
+    //
+    public $timestamps = false;
+}
